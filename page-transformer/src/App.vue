bd947737eb449e58088a0df6e758b831b7d2f4e8
p_jinzjzhou <p_jinzjzhou@tencent.com>
2022-10-08 17:42:24 +0800

    feat: lint部分代码

--- page-transformer/src/App.vue
+++ page-transformer/src/App.vue
@@ -1,24 +1,24 @@
 <script>
-  import Layout from './components/layout.vue'
-  import Grid from './components/grid.vue'
-  import FlexGrid from './components/flex-grid.vue'
+import Layout from "./components/layout.vue";
+import Grid from "./components/grid.vue";
+import FlexGrid from "./components/flex-grid.vue";
 
-  export default {
-    props: {
-        page: Array
-    },
-    components: {
-      'grid': Grid,
-      'flex-grid': FlexGrid,
-      Layout
-    },
-  }
+export default {
+  props: {
+    page: Array,
+  },
+  components: {
+    grid: Grid,
+    "flex-grid": FlexGrid,
+    Layout,
+  },
+};
 </script>
 
 <template>
   <Layout>
     <template #default>
-      <div v-for="floor in page">
+      <div v-for="(floor, index) in page" :key="index">
         <component :floor="floor" :is="floor.type"></component>
       </div>
     </template>
