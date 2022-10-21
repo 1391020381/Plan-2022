388a73c2ebb2e86dd388ca6a0279363fdf4c803a
1391020381 <1391020381@qq.com>
2022-10-21 17:27:10 +0800

    feat: nest-docs-swagger

---
+++ PullRequest的命令行管理.md
@@ -0,0 +1,6 @@
+- [Pull Request 的命令行管理](https://www.ruanyifeng.com/blog/2017/07/pull_request.html)
+
+* Pull Request 是一种通知机制。你修改了他人代码,将你的修改通知原来的作者,希望他合并你的修改,这就是 Pull Request。
+* Pull Request 本质上是一种软件的合作方式,是将涉及不同功能的代码,纳入主干的一种流程。这个流程中,还可以进行讨论 审核和修改代码。
+
+# Pull Request 的流程
--- nest-swagger/src/activity/interfaces/activity.interface.ts
+++ nest-swagger/src/activity/interfaces/activity.interface.ts
@@ -1,95 +1,95 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class Icon {
-    @ApiProperty()
-    type: string;
+  @ApiProperty()
+  type: string;
 
-    @ApiProperty({
-        description: 'icon背景颜色'
-    })
-    bg_color: string;
+  @ApiProperty({
+    description: 'icon背景颜色',
+  })
+  bg_color: string;
 
-    @ApiProperty({
-        description: 'icon边框颜色'
-    })
-    border_color: string;
+  @ApiProperty({
+    description: 'icon边框颜色',
+  })
+  border_color: string;
 
-    @ApiProperty({
-        description: 'icon字体颜色'
-    })
-    font_color: string;
+  @ApiProperty({
+    description: 'icon字体颜色',
+  })
+  font_color: string;
 
-    @ApiProperty({
-        description: 'icon文字信息'
-    })
-    text: string;
+  @ApiProperty({
+    description: 'icon文字信息',
+  })
+  text: string;
 
-    @ApiProperty()
-    source: string;
+  @ApiProperty()
+  source: string;
 }
 
 export class Offer {
-    @ApiProperty({
-        description: '商品id'
-    })
-    nid: string;
+  @ApiProperty({
+    description: '商品id',
+  })
+  nid: string;
 
-    @ApiProperty({
-        description: '商品标题'
-    })
-    title: string;
+  @ApiProperty({
+    description: '商品标题',
+  })
+  title: string;
 
-    @ApiProperty({
-        description: '商品图片地址'
-    })
-    pict_url: string;
+  @ApiProperty({
+    description: '商品图片地址',
+  })
+  pict_url: string;
 
-    @ApiProperty({
-        type: [Icon],
-        description: 'icon',
-        required: false
-    })
-    icons?: Icon[];
+  @ApiProperty({
+    type: [Icon],
+    description: 'icon',
+    required: false,
+  })
+  icons?: Icon[];
 }
 
 export class SericeError {
-    @ApiProperty({
-        description: '错误代码'
-    })
-    code: number
-    @ApiProperty({
-        description: '错误描述'
-    })
-    description: string;
+  @ApiProperty({
+    description: '错误代码',
+  })
+  code: number;
+  @ApiProperty({
+    description: '错误描述',
+  })
+  description: string;
 }
 
 export class Paginated<TData> {
-    @ApiProperty({
-        description: '总数量'
-    })
-    total: number;
+  @ApiProperty({
+    description: '总数量',
+  })
+  total: number;
 
-    @ApiProperty()
-    limit: number;
-  
-    @ApiProperty({
-        description: '位移'
-    })
-    offset: number;
-  
-    results: TData[];
+  @ApiProperty()
+  limit: number;
 
-    @ApiProperty({
-        description: '错误列表',
-        type: [SericeError]
-    })
-    errors: SericeError[]
+  @ApiProperty({
+    description: '位移',
+  })
+  offset: number;
 
-    constructor(data: TData[], offset: number = 0, limit: number = 10) {
-        this.limit = limit;
-        this.offset = offset;
-        this.total = data.length;
-        this.errors = []
-        this.results = data.slice(this.offset, this.offset + limit)
-    }
+  results: TData[];
+
+  @ApiProperty({
+    description: '错误列表',
+    type: [SericeError],
+  })
+  errors: SericeError[];
+
+  constructor(data: TData[], offset = 0, limit = 10) {
+    this.limit = limit;
+    this.offset = offset;
+    this.total = data.length;
+    this.errors = [];
+    this.results = data.slice(this.offset, this.offset + limit);
+  }
 }
