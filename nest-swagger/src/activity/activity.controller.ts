eba3497dc29b673210a3189567e62a3fc2ee7a6f
1391020381 <1391020381@qq.com>
2022-10-21 15:25:14 +0800

    feat: nest-swagger

--- nest-swagger/src/activity/activity.controller.ts
+++ nest-swagger/src/activity/activity.controller.ts
@@ -1,31 +1,34 @@
 import { Controller, Get, Param, applyDecorators, Type } from '@nestjs/common';
-import { ApiTags, ApiOkResponse, getSchemaPath, ApiExtraModels } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOkResponse,
+  getSchemaPath,
+  ApiExtraModels,
+} from '@nestjs/swagger';
 import { ActivityService } from './activity.service';
 import { Offer, Paginated, SericeError } from './interfaces/activity.interface';
 
-
 export const ApiPaginatedResponse = <TModel extends Type<any>>(
-        model: TModel,
-    ) => {
-        return applyDecorators(
-            ApiOkResponse({
-                schema: {
-                    allOf: [
-                        { $ref: getSchemaPath(Paginated) },
-                        {
-                            properties: {
-                                results: {
-                                    type: 'array',
-                                    items: { $ref: getSchemaPath(model) },
-                                },
-                            },
-                        },
-                    ],
-                },
-            }),
-        );
-    };
-
+  model: TModel,
+) => {
+  return applyDecorators(
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(Paginated) },
+          {
+            properties: {
+              results: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model) },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
+};
 
 @ApiTags('activity')
 @Controller('activity')
@@ -33,11 +36,11 @@ export const ApiPaginatedResponse = <TModel extends Type<any>>(
 @ApiExtraModels(Offer)
 @ApiExtraModels(SericeError)
 export class ActivityController {
-    constructor(private readonly activityService: ActivityService) {}
+  constructor(private readonly activityService: ActivityService) {}
 
-    @Get(':id')
-    @ApiPaginatedResponse(Offer)
-    findOne(@Param('id') id: string): Paginated<Offer> {
-        return new Paginated(this.activityService.findOne(id), 0, 10)
-    }
+  @Get(':id')
+  @ApiPaginatedResponse(Offer)
+  findOne(@Param('id') id: string): Paginated<Offer> {
+    return new Paginated(this.activityService.findOne(id), 0, 10);
+  }
 }
