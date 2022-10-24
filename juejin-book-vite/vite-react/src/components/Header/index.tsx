fec8372c712095245d0ca61e5685ee68160edc72
1391020381 <1391020381@qq.com>
2022-10-24 15:22:46 +0800

    tailwind fail

--- juejin-book-vite/vite-react/src/components/Header/index.tsx
+++ juejin-book-vite/vite-react/src/components/Header/index.tsx
@@ -1,4 +1,16 @@
 import styles from "./index.module.scss";
+import { devDependencies } from "../../../package.json";
 export function Header() {
-  return <p className={styles.header}>This is Header</p>;
+  return (
+    // <div className={styles.header}>
+    //   <p className={styles.header}>This is Header</p>
+    //   <p className="p-20px text-center">
+    //     <h1 className="font-bold text-2xl mb-2">
+    //       {" "}
+    //       vite version:{devDependencies.vite}
+    //     </h1>
+    //   </p>
+    // </div>
+    <p className={styles.header}>This is Header</p>
+  );
 }
--- juejin-book-vite/vite-react/vite.config.ts
+++ juejin-book-vite/vite-react/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, normalizePath } from "vite";
 // 如果类型报错, 需要安装 @types/node  pnpm i @types/node -D
 import path from "path";
 import react from "@vitejs/plugin-react";
+import winddi from "vite-plugin-windicss";
 // 全局 scss文件的路径
 // 用 normalizePath 解决 window下的路径问题
 const variablePath = normalizePath(path.resolve("./src/variable.scss"));
@@ -20,5 +21,20 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          // // 适配 styled-component
+          // "babel-plugin-styled-components",
+          // // 适配 emotion
+          // "@emotion/babel-plugin",
+        ],
+      },
+      // 通过 `@emotion/react` 包编译 emotion 中的特殊 jsx 语法
+      // jsxRuntime: "automatic", // <---
+      // jsxImportSource: "@emotion/react",
+    }),
+    winddi(),
+  ],
 });
