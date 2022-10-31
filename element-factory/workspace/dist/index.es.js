ee16a1ac886def99e9f467edf043a6f5d3cc8f86
1391020381 <1391020381@qq.com>
2022-10-31 17:26:10 +0800

    element-factory

--- element-factory/workspace/dist/index.es.js
+++ element-factory/workspace/dist/index.es.js
@@ -1,103 +1,76 @@
-import { openBlock, createElementBlock, createElementVNode, renderSlot, createTextVNode, toDisplayString, Fragment, renderList, normalizeStyle, createBlock, withCtx, createVNode } from "vue";
-var card_vue_vue_type_style_index_0_scope_true_lang = "";
-const _hoisted_1$3 = {
+import { openBlock as c, createElementBlock as n, createElementVNode as _, renderSlot as l, createTextVNode as a, toDisplayString as u, Fragment as g, renderList as v, normalizeStyle as m, createBlock as h, withCtx as i, createVNode as p } from "vue";
+const y = {
   class: "card-item",
   role: "listitem"
-};
-const _hoisted_2$2 = ["href"];
-const _hoisted_3 = { class: "img-wrapper" };
-const _hoisted_4 = /* @__PURE__ */ createTextVNode("image slot");
-const _hoisted_5 = { class: "info-wrapper" };
-const _hoisted_6 = { class: "title" };
-const _hoisted_7 = /* @__PURE__ */ createTextVNode("title slot");
-const _hoisted_8 = /* @__PURE__ */ createTextVNode("under title slot");
-const _hoisted_9 = { class: "price-wrapper" };
-const _hoisted_10 = /* @__PURE__ */ createTextVNode("price slot");
-const _sfc_main$4 = {
+}, f = ["href"], $ = { class: "img-wrapper" }, k = /* @__PURE__ */ a("image slot"), b = { class: "info-wrapper" }, x = { class: "title" }, S = /* @__PURE__ */ a("title slot"), w = /* @__PURE__ */ a("under title slot"), B = { class: "price-wrapper" }, I = /* @__PURE__ */ a("price slot"), N = {
   __name: "card",
   props: {
     link: String
   },
-  setup(__props) {
-    const props = __props;
-    return (_ctx, _cache) => {
-      return openBlock(), createElementBlock("div", _hoisted_1$3, [
-        createElementVNode("a", {
-          href: props.link,
-          target: "_blank",
-          class: "item-link"
-        }, [
-          createElementVNode("div", _hoisted_3, [
-            renderSlot(_ctx.$slots, "image", {}, () => [
-              _hoisted_4
+  setup(e) {
+    const t = e;
+    return (s, r) => (c(), n("div", y, [
+      _("a", {
+        href: t.link,
+        target: "_blank",
+        class: "item-link"
+      }, [
+        _("div", $, [
+          l(s.$slots, "image", {}, () => [
+            k
+          ])
+        ]),
+        _("div", b, [
+          _("div", x, [
+            l(s.$slots, "title", {}, () => [
+              S
             ])
           ]),
-          createElementVNode("div", _hoisted_5, [
-            createElementVNode("div", _hoisted_6, [
-              renderSlot(_ctx.$slots, "title", {}, () => [
-                _hoisted_7
-              ])
-            ]),
-            createElementVNode("div", null, [
-              renderSlot(_ctx.$slots, "under-title", {}, () => [
-                _hoisted_8
-              ])
-            ])
-          ]),
-          createElementVNode("div", _hoisted_9, [
-            renderSlot(_ctx.$slots, "price", {}, () => [
-              _hoisted_10
+          _("div", null, [
+            l(s.$slots, "under-title", {}, () => [
+              w
             ])
           ])
-        ], 8, _hoisted_2$2)
-      ]);
-    };
+        ]),
+        _("div", B, [
+          l(s.$slots, "price", {}, () => [
+            I
+          ])
+        ])
+      ], 8, f)
+    ]));
   }
 };
-var rectPic_vue_vue_type_style_index_0_scoped_15225c77_lang = "";
-var _export_sfc = (sfc, props) => {
-  const target = sfc.__vccOpts || sfc;
-  for (const [key, val] of props) {
-    target[key] = val;
-  }
-  return target;
-};
-const _hoisted_1$2 = { class: "img-container" };
-const _hoisted_2$1 = ["src"];
-const _sfc_main$3 = {
+const d = (e, t) => {
+  const s = e.__vccOpts || e;
+  for (const [r, o] of t)
+    s[r] = o;
+  return s;
+}, T = { class: "img-container" }, V = ["src"], C = {
   __name: "rect-pic",
   props: {
     value: String
   },
-  setup(__props) {
-    const props = __props;
-    return (_ctx, _cache) => {
-      return openBlock(), createElementBlock("div", _hoisted_1$2, [
-        createElementVNode("img", {
-          src: props.value
-        }, null, 8, _hoisted_2$1)
-      ]);
-    };
+  setup(e) {
+    const t = e;
+    return (s, r) => (c(), n("div", T, [
+      _("img", {
+        src: t.value
+      }, null, 8, V)
+    ]));
   }
-};
-var RectPic = /* @__PURE__ */ _export_sfc(_sfc_main$3, [["__scopeId", "data-v-15225c77"]]);
-var title_vue_vue_type_style_index_0_scoped_c9d794a6_lang = "";
-const _sfc_main$2 = {
+}, E = /* @__PURE__ */ d(C, [["__scopeId", "data-v-88e43097"]]);
+const O = {
   __name: "title",
   props: {
     msg: String
   },
-  setup(__props) {
-    const props = __props;
-    return (_ctx, _cache) => {
-      return openBlock(), createElementBlock("p", null, toDisplayString(props.msg), 1);
-    };
+  setup(e) {
+    const t = e;
+    return (s, r) => (c(), n("p", null, u(t.msg), 1));
   }
-};
-var Title = /* @__PURE__ */ _export_sfc(_sfc_main$2, [["__scopeId", "data-v-c9d794a6"]]);
-var tags_vue_vue_type_style_index_0_scoped_af5c5eb3_lang = "";
-const _hoisted_1$1 = { class: "tag-list" };
-const _sfc_main$1 = {
+}, P = /* @__PURE__ */ d(O, [["__scopeId", "data-v-c657ab2c"]]);
+const j = { class: "tag-list" }, z = {
   __name: "tags",
   props: {
     tags: {
@@ -105,65 +78,50 @@ const _sfc_main$1 = {
       default: () => []
     }
   },
-  setup(__props) {
-    const props = __props;
-    return (_ctx, _cache) => {
-      return openBlock(), createElementBlock("div", _hoisted_1$1, [
-        (openBlock(true), createElementBlock(Fragment, null, renderList(props.tags, (item) => {
-          return openBlock(), createElementBlock("div", {
-            class: "tag-item",
-            key: item,
-            style: normalizeStyle({ color: item.font_color, borderColor: item.border_color, background: item.bg_color })
-          }, toDisplayString(item.text), 5);
-        }), 128))
-      ]);
-    };
+  setup(e) {
+    const t = e;
+    return (s, r) => (c(), n("div", j, [
+      (c(!0), n(g, null, v(t.tags, (o) => (c(), n("div", {
+        class: "tag-item",
+        key: o,
+        style: m({ color: o.font_color, borderColor: o.border_color, background: o.bg_color })
+      }, u(o.text), 5))), 128))
+    ]));
   }
-};
-var Tags = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["__scopeId", "data-v-af5c5eb3"]]);
-var price_vue_vue_type_style_index_0_lang = "";
-const _hoisted_1 = { class: "price-value" };
-const _hoisted_2 = /* @__PURE__ */ createElementVNode("em", null, "\xA5", -1);
-const _sfc_main = {
+}, A = /* @__PURE__ */ d(z, [["__scopeId", "data-v-8b89c5a6"]]);
+const D = { class: "price-value" }, F = /* @__PURE__ */ _("em", null, "\xA5", -1), L = {
   __name: "price",
   props: {
     value: String
   },
-  setup(__props) {
-    const props = __props;
-    return (_ctx, _cache) => {
-      return openBlock(), createElementBlock("span", _hoisted_1, [
-        _hoisted_2,
-        createTextVNode(toDisplayString(props.value), 1)
-      ]);
-    };
+  setup(e) {
+    const t = e;
+    return (s, r) => (c(), n("span", D, [
+      F,
+      a(u(t.value), 1)
+    ]));
   }
-};
-const __default__ = {
-  data: (el) => {
-    console.log("data === ", el.$attrs);
-    return el.$attrs;
-  }
-};
-var app = /* @__PURE__ */ Object.assign(__default__, {
-  setup(__props) {
-    return (_ctx, _cache) => {
-      return openBlock(), createBlock(_sfc_main$4, { link: _ctx.click_url }, {
-        image: withCtx(() => [
-          createVNode(RectPic, { value: _ctx.pict_url }, null, 8, ["value"])
-        ]),
-        title: withCtx(() => [
-          createVNode(Title, { msg: _ctx.title }, null, 8, ["msg"])
-        ]),
-        "under-title": withCtx(() => [
-          createVNode(Tags, { tags: _ctx.icons }, null, 8, ["tags"])
-        ]),
-        price: withCtx(() => [
-          createVNode(_sfc_main, { value: _ctx.real_wap_price }, null, 8, ["value"])
-        ]),
-        _: 1
-      }, 8, ["link"]);
-    };
+}, R = {
+  data: (e) => (console.log("data === ", e.$attrs), e.$attrs)
+}, G = /* @__PURE__ */ Object.assign(R, {
+  setup(e) {
+    return (t, s) => (c(), h(N, { link: t.click_url }, {
+      image: i(() => [
+        p(E, { value: t.pict_url }, null, 8, ["value"])
+      ]),
+      title: i(() => [
+        p(P, { msg: t.title }, null, 8, ["msg"])
+      ]),
+      "under-title": i(() => [
+        p(A, { tags: t.icons }, null, 8, ["tags"])
+      ]),
+      price: i(() => [
+        p(L, { value: t.real_wap_price }, null, 8, ["value"])
+      ]),
+      _: 1
+    }, 8, ["link"]));
   }
 });
-export { app as default };
+export {
+  G as default
+};
--- element-factory/workspace/vite.config.js
+++ element-factory/workspace/vite.config.js
@@ -1,47 +1,46 @@
-import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
-import { resolve, dirname } from 'path'
-import vue from '@vitejs/plugin-vue'
-import compile from '../src/index.mjs'
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import { resolve, dirname } from "path";
+import vue from "@vitejs/plugin-vue";
+import compile from "../src/index.mjs";
 
 function plugin() {
-    return {
-        name: 'transform-tpl',
-        transform(src, id) {
-            if (/\.tpl$/.test(id)) {
-                const code = compile(src, {})
-                return { code }
-            }
-        }
-    }
+  return {
+    name: "transform-tpl",
+    transform(src, id) {
+      if (/\.tpl$/.test(id)) {
+        const code = compile(src, {});
+        return { code };
+      }
+    },
+  };
 }
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), plugin()],
   build: {
     rollupOptions: {
-        external: [
-            'vue',
-        ],
-        output: {
-            globals: {
-                'vue': 'Vue'
-            },
+      // 有时候一些外部引用的库我们并不想一并打包到我们的库中,如 lodash react 可以在 配置文件中使用 external字段来告诉rollup不要将这些库打包
+      external: ["vue"],
+      output: {
+        globals: {
+          //
+          vue: "Vue",
         },
+      },
     },
     lib: {
-        entry: './app.tpl',
-        name: 'MyLib',
-        // the proper extensions will be added
-        fileName: (format) => `index.${format}.js`,
-        formats: ['es', 'amd', 'umd'],
-    }
+      entry: "./app.tpl",
+      name: "MyLib",
+      // the proper extensions will be added
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "amd", "umd"],
+    },
   },
   resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-  }
-})
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
