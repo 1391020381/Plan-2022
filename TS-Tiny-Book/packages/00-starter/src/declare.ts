3b6d02f164b832a39f29c671043008886c206d69
1391020381 <1391020381@qq.com>
2023-01-03 15:30:25 +0800

    00-starter

---
+++ TS-Tiny-Book/packages/00-starter/src/declare.ts
@@ -0,0 +1,4 @@
+declare let v1:string;
+declare let v2:string;
+v1 = v2;
+v2 = v1;
---
+++ TS-Tiny-Book/packages/00-starter/src/index.ts
@@ -0,0 +1,3 @@
+import { expectType} from 'tsd';
+expectType<string>('linbudu');
+console.log('Your TypeScript Playground works!')
