59fc393c117f9669b9277e54a5837ed635e5b35c
1391020381 <1391020381@qq.com>
2023-02-09 18:16:43 +0800

    typeorm

--- nestjs-overview/src/users/users.service.ts
+++ nestjs-overview/src/users/users.service.ts
@@ -24,7 +24,7 @@ export class UsersService {
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.usersRepository.update(id,updateUserDto)
   }
 
   remove(id: string):Promise<DeleteResult> {
