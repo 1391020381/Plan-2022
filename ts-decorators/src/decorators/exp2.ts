917247371079215c8c1305b25980b04d84070550
1391020381 <1391020381@qq.com>
2023-02-27 17:56:39 +0800

    exp2

---
+++ ts-decorators/src/decorators/exp2.ts
@@ -0,0 +1,32 @@
+const HelloDerorator = <T extends new (...args: any[]) => any>(
+  constructor: T,
+) => {
+  return class extends constructor {
+    newProperty = 'new property';
+    hello = 'override';
+    sayHello() {
+      return this.hello;
+    }
+  };
+};
+
+@HelloDerorator
+export class Hello {
+  [key: string]: any;
+  hello: string;
+  constructor() {
+    this.hello = 'test';
+  }
+}
+
+const exp2 = () => {
+  console.log('-----------示例2:简单的类装饰器-----------------');
+  console.log('------------动态添加一个sayHello方法以及覆盖hello的值');
+  console.log();
+  const hello = new Hello();
+  console.log(hello.sayHello());
+  console.log();
+  console.log('------------示例2:执行完毕');
+};
+
+export { exp2 };
--- ts-decorators/src/main.ts
+++ ts-decorators/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { exp2 } from './decorators/exp2';
 async function bootstrap() {
+  exp2();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
