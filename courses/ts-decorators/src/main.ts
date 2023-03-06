4bfee1fcc7092c054dcf96056f2bb3916e44571a
1391020381 <1391020381@qq.com>
2023-03-06 11:10:22 +0800

    feat: TS装饰器

--- courses/ts-decorators/src/main.ts
+++ courses/ts-decorators/src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { exp1 } from './decorators/exp1';
 import { exp910 } from './decorators/exp9-10';
 
 async function bootstrap() {
-    // exp1();
+    exp1();
     // exp2();
     // exp3();
     // exp4();
     // exp5();
     // exp6();
-    exp910();
+    // exp910();
     // exp910();
     // exp11();
     const app = await NestFactory.create(AppModule, { logger: false });
