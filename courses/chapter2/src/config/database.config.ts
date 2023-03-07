8be41214b15277a3dda8bc4673b31648d7635402
1391020381 <1391020381@qq.com>
2023-03-07 17:23:29 +0800

    chapter2

--- courses/chapter2/src/config/database.config.ts
+++ courses/chapter2/src/config/database.config.ts
@@ -12,6 +12,8 @@ export const database = (): TypeOrmModuleOptions => ({
     username: 'root',
     password: '171226q2',
     database: '3r',
+    // 因为我们目前没有涉及到数据迁移到命令编写,所以必须在启动数据库时自动根据加载的模块(Entity)来同步数据到数据库。
     synchronize: true,
+    // 这样我们就不需要把每个模块的Entity逐个定死地添加到配置中 entites数组中，因为你可以在每个模块中使用TypeOrmModule.forFeature来动态的加入Entity
     autoLoadEntities: true,
 });
---
+++ notes/chapter2.md
@@ -0,0 +1,16 @@
+# 数据库模块
+
+## 动态模块
+
+- src/modules/database
+
+* database.module.ts
+* app.moudule.ts 注入 database 和 content 模块
+
+## 自定义 Repository
+
+- 因为我们使用的是 DataMapper 与 ActiveRecord 混合的方式来编写数据操作。
+
+* [CustomRepository](https://gist.github.com/anchan828/9e569f076e7bc18daf21c652f7c3d012)
+* 数据实体注入
+* [Database](https://docs.nestjs.com/techniques/database)
