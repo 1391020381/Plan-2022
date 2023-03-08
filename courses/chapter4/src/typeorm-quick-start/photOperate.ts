2705a18fdde047cdc0a6166e5f18a38079dafed2
1391020381 <1391020381@qq.com>
2023-03-08 16:04:58 +0800

    feat: cascade查询

--- courses/chapter4/src/typeorm-quick-start/photOperate.ts
+++ courses/chapter4/src/typeorm-quick-start/photOperate.ts
@@ -4,37 +4,73 @@ import { Photo } from './entity/Photo';
 import { PhotoMetadata } from './entity/PhotoMetadata';
 
 const photOperate = async () => {
-    // create a photo
+    // // create a photo
+    // const photo = new Photo();
+    // photo.name = 'Me and Bears';
+    // photo.description = 'I am near polar bears';
+    // photo.filename = 'photo-with-bears.jpg';
+    // photo.views = 1;
+    // photo.isPublished = true;
+
+    // // create a photo metadata
+    // const metadata = new PhotoMetadata();
+    // metadata.height = 640;
+    // metadata.width = 480;
+    // metadata.compressed = true;
+    // metadata.comment = 'cybershoot';
+    // metadata.orientation = 'portrait';
+    // metadata.photo = photo; // this way we connect them
+    // // get entity repositories
+    // const photoRepository = AppDataSource.getRepository(Photo);
+    // const metadataRepository = AppDataSource.getRepository(PhotoMetadata);
+    // // console.log('metadataRepository:', metadataRepository);
+    // // first we should save a photo
+    // await photoRepository.save(photo);
+
+    // // photo is saved. Now we need to save a photo metadata
+    // await metadataRepository.save(metadata);
+    // const photos = await photoRepository.find({ relations: ['metadata'] });
+    // console.log('photos:', photos);
+    // // done
+    // console.log(
+    //     'Metadata is saved, and the relation between metadata and photo is created in the database too',
+    // );
+
+    // create photo object
     const photo = new Photo();
     photo.name = 'Me and Bears';
-    photo.description = 'I am near polar bears';
+    photo.description = 'I am near polar bears------xxxxxxxxxxxxxx';
     photo.filename = 'photo-with-bears.jpg';
     photo.views = 1;
     photo.isPublished = true;
 
-    // create a photo metadata
+    // create photo metadata object
     const metadata = new PhotoMetadata();
     metadata.height = 640;
     metadata.width = 480;
     metadata.compressed = true;
-    metadata.comment = 'cybershoot';
+    metadata.comment = 'cybershoot-------xxxxxxxxxxx';
     metadata.orientation = 'portrait';
-    metadata.photo = photo; // this way we connect them
-    // get entity repositories
-    const photoRepository = AppDataSource.getRepository(Photo);
-    const metadataRepository = AppDataSource.getRepository(PhotoMetadata);
-    // console.log('metadataRepository:', metadataRepository);
-    // first we should save a photo
-    await photoRepository.save(photo);
+    // metadata.photo = photo;
+    photo.metadata = metadata; // this way we connect them
 
-    // photo is saved. Now we need to save a photo metadata
-    await metadataRepository.save(metadata);
-    const photos = await photoRepository.find({ relations: ['metadata'] });
-    console.log('photos:', photos);
-    // done
-    console.log(
-        'Metadata is saved, and the relation between metadata and photo is created in the database too',
-    );
+    // get repository
+    const photoRepository = AppDataSource.getRepository(Photo);
+
+    // saving a photo also save the metadata
+    await photoRepository.save(photo);
+    // const photos = await photoRepository.find({
+    //     relations: {
+    //         metadata: true,
+    //     },
+    // });
+    const photosMetaDataRepository = AppDataSource.getRepository(PhotoMetadata);
+    const photosMeta = await photosMetaDataRepository.find({
+        relations: {
+            photo: true,
+        },
+    });
+    console.log('关联存储与关联查询:', photosMeta);
 };
 
 export { photOperate };
