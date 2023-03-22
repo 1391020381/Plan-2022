2157d30a6661905289b9e86feb407332cfabd88d
1391020381 <1391020381@qq.com>
2023-03-22 15:58:13 +0800

    feat: ts

--- courses/TypeScript-Tiny-Book-demo/packages/01-primitive-and-object/src/primitive.ts
+++ courses/TypeScript-Tiny-Book-demo/packages/01-primitive-and-object/src/primitive.ts
@@ -1,15 +1,17 @@
-const name: string = 'linbudu';
+const name: string = "linbudu";
 const age: number = 24;
 const male: boolean = false;
 const undef: undefined = undefined;
 const nul: null = null;
 const obj: object = { name, age, male };
 const bigintVar1: bigint = 9007199254740991n;
-const bigintVar2: bigint = BigInt(9007199254740991);
-const symbolVar: symbol = Symbol('unique');
+const biginVar2: bigint = BigInt(9007199254740991);
+const symbolVar: symbol = Symbol("unique");
+
+// 在没有开启 stricNullChecks 检查的情况下,会被视作其他类型的子类型,比如 string类型会被认为包含了 null 与 undefined类型。
 
 const tmp1: null = null;
 const tmp2: undefined = undefined;
-// 以下两个仅在关闭 strictNullChecks 时成立
+
 const tmp3: string = null;
-const tmp4: string = undefined;
+const temp4: string = undefined;
